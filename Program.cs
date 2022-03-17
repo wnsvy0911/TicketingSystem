a3bf80d348aa8b7f4d5660605e46dc9ef29e68e8
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-17 14:33:45 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
