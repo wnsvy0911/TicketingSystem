397e1ddd208c4bda62553e46c16cea5d7fd606bb
Junpyo Kim <jpkimkorea@gmail.com>
2022-03-17 14:52:53 -0500

    Add TicketsFile

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Collections.Generic;
 
 namespace TicketingSystem
 {
@@ -10,11 +11,86 @@ namespace TicketingSystem
         static void Main(string[] args)
         {
 
-            logger.Info("Program started");
+            string file = "Tickets.csv";
+            StreamReader sr = new StreamReader(file);
+                List<string> originalFile = new List<string>();
+                while (!sr.EndOfStream)
+                {
+                    originalFile.Add(sr.ReadLine());
+                    
+                }
+                sr.Close();
 
-            Console.WriteLine("Hello World!");
+              string choice;
+            do
+            {
+                Console.WriteLine("1) Read data from CSV file.");
+                Console.WriteLine("2) Create file from data.");
+                Console.WriteLine("Enter any other key to exit.");
+                choice = Console.ReadLine();
 
-            logger.Info("Program ended");
+                if (choice == "1")
+                {
+                    if (File.Exists(file))
+                    {
+                     
+                        StreamReader sr1 = new StreamReader(file);
+
+                        while (!sr1.EndOfStream)
+                        {
+                            string line = sr1.ReadLine();
+                            string[] arr = line.Split(',');
+                         
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6}",
+                            arr[0],arr[1] , arr[2] , arr[3] , arr[4],arr[5], arr[6]);
+                        }
+                        sr1.Close();
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+                }
+                else if (choice == "2")
+                {
+                    
+                    StreamWriter sw = new StreamWriter(file);
+    
+                    Console.WriteLine("Do you want to create more data?(Y/N)");
+                    string resp = Console.ReadLine().ToUpper();
+
+                    if (resp != "Y") { break; }
+                    Console.WriteLine("What is the TicketID (Type only number)?");
+                    int ticketID = Convert.ToInt32(Console.ReadLine());                        
+                    
+                    Console.WriteLine("Enter a summary.");                        
+                    string summary = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter the status(Open/Close)");
+                    string status = Console.ReadLine();
+                    
+                    Console.WriteLine("Enter the priority");   
+                    string priority = Console.ReadLine();
+
+                    Console.WriteLine("Enter the submitter");
+                    string submitter = Console.ReadLine();
+
+                    Console.WriteLine("Enter the assigned");
+                    string assigned = Console.ReadLine();
+
+                    Console.WriteLine("Enter the watching");
+                    string watching = Console.ReadLine();
+                    
+                    originalFile.Add(ticketID + "," + summary + "," + status + "," + priority + "," + submitter + "," + assigned + "," + watching);
+
+                    foreach (var line in originalFile) {
+                        sw.WriteLine(line);
+                    }
+                    
+                    sw.Close();
+                }
+            } while (choice == "1" || choice == "2");
         }   
     }
 }
